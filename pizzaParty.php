489ca3dd9e52d97c99c70b1538ad1f7a69ddc4f2
meherin sohani <msohani2330420@bscse.uiu.ac.bd>
2026-02-08 16:37:03 +0600

    pizza changes

--- pizzaParty.php
+++ pizzaParty.php
@@ -1,65 +1,54 @@
 <?php
-function pizzaCalc($students, $slicesPerStudent, $slicesPerPizza)
+
+function pizzaParty($students, $slicesPerStudent, $slicesPerPizza)
 {
     $pizzaPrice = 1050;
 
-    $neededSlices = $students * $slicesPerStudent;
+    // total slices needed
+    $totalSlicesNeeded = $students * $slicesPerStudent;
 
-    $totalPizzas = (int) ceil($neededSlices / $slicesPerPizza);
+    // total pizzas needed (must order whole pizzas)
+    $totalPizzas = ceil($totalSlicesNeeded / $slicesPerPizza);
 
-    $totalSlices = $totalPizzas * $slicesPerPizza;
+    // total slices available
+    $totalSlicesAvailable = $totalPizzas * $slicesPerPizza;
 
-    $leftoverSlices = $totalSlices - $neededSlices;
+    // leftover slices
+    $leftoverSlices = $totalSlicesAvailable - $totalSlicesNeeded;
 
+    // wasted money based on leftover slices
     $wastedMoney = ($leftoverSlices / $slicesPerPizza) * $pizzaPrice;
-
-    return [$totalPizzas, $leftoverSlices, $wastedMoney];
-}
-
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-
-    $cases = [
-        [ (int)$_POST["students1"], (int)$_POST["sps1"], (int)$_POST["spp1"] ],
-        [ (int)$_POST["students2"], (int)$_POST["sps2"], (int)$_POST["spp2"] ],
-        [ (int)$_POST["students3"], (int)$_POST["sps3"], (int)$_POST["spp3"] ],
-    ];
-
-    echo "<!DOCTYPE html><html><head><title>Pizza Party Output</title>
-    <style>
-      table { border-collapse: collapse; width: 70%; margin: 20px auto; }
-      th, td { border: 1px solid #000; padding: 10px; text-align: center; }
-      th { background: #eee; }
-      h2 { text-align: center; }
-      a { display:block; text-align:center; margin-top:20px; }
-    </style>
-    </head><body>";
 
     echo "<h2>Pizza Party Output</h2>";
 
-    echo "<table>";
+    echo "<table border='1' cellpadding='10' cellspacing='0'>";
     echo "<tr>
             <th>Total Pizzas</th>
             <th>Leftover Slices</th>
             <th>Wasted Money (BDT)</th>
           </tr>";
 
-    foreach ($cases as $c) {
-        [$pizzas, $leftover, $wasted] = pizzaCalc($c[0], $c[1], $c[2]);
-
-        $wastedFormatted = (fmod($wasted, 1.0) == 0.0) ? number_format($wasted, 0) : number_format($wasted, 1);
-
-        echo "<tr>
-                <td>$pizzas</td>
-                <td>$leftover</td>
-                <td>$wastedFormatted</td>
-              </tr>";
-    }
+    echo "<tr>
+            <td>$totalPizzas</td>
+            <td>$leftoverSlices</td>
+            <td>$wastedMoney</td>
+          </tr>";
 
     echo "</table>";
-    echo "<a href='indexPizza.html'>Go Back</a>";
 
-    echo "</body></html>";
-} else {
-    echo "Please open <a href='indexPizza.html'>indexPizza.html</a> and submit the form.";
+    echo "<br><a href='indexPizza.html'>Go Back</a>";
 }
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+    $students = (int) $_POST["students"];
+    $slicesPerStudent = (int) $_POST["slicesPerStudent"];
+    $slicesPerPizza = (int) $_POST["slicesPerPizza"];
+
+    pizzaParty($students, $slicesPerStudent, $slicesPerPizza);
+
+} else {
+    echo "Please submit the form from <a href='indexPizza.html'>indexPizza.html</a>";
+}
+
 ?>
